867c5eecb13dc17788cd2581dc2619b8b04f7553
Rajat Monga <rajatmonga29@gmail.com>
2018-05-01 00:01:18 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,7 +4,19 @@ void main() => runApp(new myApp());
 
 class myApp extends StatelessWidget {
 
-  return null;
+  Widget build(BuildContext context) {
+    title:"Title",
+    home: new Scaffold(
+      appBar: new AppBar (
+        title: new Text("App Bar Title"),
+      ),
+      body: new Center(
+        child: new Text("Scaffold"),
 
+      ),//Center
 
+    )//Scaffold
+
+   ); //MaterialApp
+ }
 }
